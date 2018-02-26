b3919f5b362a8b68c278627836a1147483a7abc4
Gustavo Escalante <escalantegc@gmail.com>
2018-02-26 20:02:44 -0300

    listado de cargos

--- php/listados/ci_listado_cargos.php
+++ php/listados/ci_listado_cargos.php
@@ -15,6 +15,9 @@ class ci_listado_cargos extends sicd_ci
 		$filtro->columna('fecha_inicio')->set_condicion_fija('es_igual_a');
 		$filtro->columna('fecha_fin')->set_condicion_fija('es_igual_a');
 		$filtro->columna('idtipo_hora')->set_condicion_fija('es_igual_a');
+		$filtro->columna('tipo')->set_condicion_fija('es_igual_a');
+		$filtro->columna('activo')->set_condicion_fija('es_igual_a');
+		$filtro->columna('historico')->set_condicion_fija('es_igual_a');
 	}
 
 
@@ -40,9 +43,14 @@ class ci_listado_cargos extends sicd_ci
 		$idtipo_cargo= '%%';
 		$identidad = '%%';
 		$cantidad_horas = '';
-		$fecha_inicio = '';
-		$fecha_fin = '';
+		$fecha_inicio = '%%';
+		$fecha_fin = '%%';	
+		$fecha_inicio_mostrar = '%%';
+		$fecha_fin_mostrar = '%%';
 		$idtipo_hora = '%%';
+		$tipo = '%%';
+		$activo = 'true';
+		$historico = 'false';
 		
 		
 		$orderby_mostrar = 'Persona';
@@ -84,12 +92,31 @@ class ci_listado_cargos extends sicd_ci
 				$fecha_inicio = utf8_encode(trim($this->s__criterios_filtrado['fecha_inicio']['valor']));
 				
 			}
+		}		
+
+		if (isset($this->s__criterios_filtrado['fecha_inicio_mostrar']['valor'])!='')
+		{
+			if (trim($this->s__criterios_filtrado['fecha_inicio_mostrar']['valor'])!='nopar')
+			{
+				$fecha_inicio_mostrar = utf8_encode(trim($this->s__criterios_filtrado['fecha_inicio_mostrar']['valor']));
+				
+			}
 		}
+		
 		if (isset($this->s__criterios_filtrado['fecha_fin']['valor'])!='')
 		{
 			if (trim($this->s__criterios_filtrado['fecha_fin']['valor'])!='nopar')
 			{
 				$fecha_fin = utf8_encode(trim($this->s__criterios_filtrado['fecha_fin']['valor']));
+				
+			}
+		}
+		
+		if (isset($this->s__criterios_filtrado['fecha_fin_mostrar']['valor'])!='')
+		{
+			if (trim($this->s__criterios_filtrado['fecha_fin_mostrar']['valor'])!='nopar')
+			{
+				$fecha_fin_mostrar = utf8_encode(trim($this->s__criterios_filtrado['fecha_fin_mostrar']['valor']));
 				
 			}
 		}		
@@ -101,21 +128,52 @@ class ci_listado_cargos extends sicd_ci
 				$idtipo_hora = utf8_encode(trim($this->s__criterios_filtrado['idtipo_hora']['valor']));
 				
 			}
+		}		
+
+		if (isset($this->s__criterios_filtrado['tipo']['valor'])!='')
+		{
+			if (trim($this->s__criterios_filtrado['tipo']['valor'])!='nopar')
+			{
+				$tipo = utf8_encode(trim($this->s__criterios_filtrado['tipo']['valor']));
+				
+			}
+		}		
+
+		if (isset($this->s__criterios_filtrado['activo']['valor'])!='')
+		{
+			if (trim($this->s__criterios_filtrado['activo']['valor'])!='nopar')
+			{
+				$activo = utf8_encode(trim($this->s__criterios_filtrado['activo']['valor']));
+				
+			}
+		}		
+		if (isset($this->s__criterios_filtrado['historico']['valor'])!='')
+		{
+			if (trim($this->s__criterios_filtrado['historico']['valor'])!='nopar')
+			{
+				$historico = utf8_encode(trim($this->s__criterios_filtrado['historico']['valor']));
+				
+			}
 		}
 			
 		$report->set_parametro('identidad', 'S', $identidad);
 		$report->set_parametro('cantidad_horas', 'S', $cantidad_horas);
 		$report->set_parametro('idtipo_cargo', 'S', $idtipo_cargo);
 		$report->set_parametro('fecha_inicio', 'S', $fecha_inicio);
-		$report->set_parametro('fecha_fin', 'S', $fecha_fin);
-		$report->set_parametro('idtipo_hora', 'S', $fecha_fin);
+		$report->set_parametro('fecha_fin', 'S', $fecha_fin);	
+		$report->set_parametro('fecha_inicio_mostrar', 'S', $fecha_inicio_mostrar);
+		$report->set_parametro('fecha_fin_mostrar', 'S', $fecha_fin_mostrar);
+		$report->set_parametro('idtipo_hora', 'S', $idtipo_hora);
+		$report->set_parametro('tipo', 'S', $tipo);
+		$report->set_parametro('activo', 'S', $activo);
+		$report->set_parametro('historico', 'S', $historico);
 		
 		$report->set_parametro('orderby_mostrar', 'S', $orderby_mostrar);
 	
 
 		//Parametros para el usuario
 		$report->set_parametro('usuario','S',toba::usuario()->get_id());
-		$report->set_nombre_archivo('listado_basico_viaticos.pdf');   	
+		$report->set_nombre_archivo('listado_cargos.pdf');   	
 		$db=toba::fuente('sicd')->get_db();
 		$report->set_conexion($db);	
 	}
@@ -151,11 +209,18 @@ class ci_listado_cargos extends sicd_ci
 	}	
 
 
-		function ajax__get_dato_filtro_fecha_inicio($fecha_inicio, toba_ajax_respuesta $respuesta)
+	function ajax__get_dato_filtro_fecha_inicio($fecha_inicio, toba_ajax_respuesta $respuesta)
 	{
 		$this->s__criterios_filtrado['fecha_inicio']['condicion'] =  'es_igual_a';
 		$this->s__criterios_filtrado['fecha_inicio']['valor'] =  $fecha_inicio;
 		$respuesta->set($fecha_inicio);	
+	}	
+
+	function ajax__get_dato_filtro_fecha_inicio_mostrar($fecha_inicio_mostrar, toba_ajax_respuesta $respuesta)
+	{
+		$this->s__criterios_filtrado['fecha_inicio_mostrar']['condicion'] =  'es_igual_a';
+		$this->s__criterios_filtrado['fecha_inicio_mostrar']['valor'] =  $fecha_inicio_mostrar;
+		$respuesta->set($fecha_inicio_mostrar);	
 	}		
 	
 	function ajax__get_dato_filtro_fecha_fin($fecha_fin, toba_ajax_respuesta $respuesta)
@@ -163,7 +228,38 @@ class ci_listado_cargos extends sicd_ci
 		$this->s__criterios_filtrado['fecha_fin']['condicion'] =  'es_igual_a';
 		$this->s__criterios_filtrado['fecha_fin']['valor'] =  $fecha_fin;
 		$respuesta->set($fecha_fin);	
+	}			
+
+	function ajax__get_dato_filtro_fecha_fin_mostrar($fecha_fin_mostrar, toba_ajax_respuesta $respuesta)
+	{
+		$this->s__criterios_filtrado['fecha_fin_mostrar']['condicion'] =  'es_igual_a';
+		$this->s__criterios_filtrado['fecha_fin_mostrar']['valor'] =  $fecha_fin_mostrar;
+		$respuesta->set($fecha_fin_mostrar);	
+	}		
+
+	function ajax__get_dato_filtro_tipo($tipo, toba_ajax_respuesta $respuesta)
+	{
+		$this->s__criterios_filtrado['tipo']['condicion'] =  'es_igual_a';
+		$this->s__criterios_filtrado['tipo']['valor'] =  $tipo;
+		$respuesta->set($tipo);	
+	}		
+
+	function ajax__get_dato_filtro_activo($activo, toba_ajax_respuesta $respuesta)
+	{
+		$this->s__criterios_filtrado['activo']['condicion'] =  'es_igual_a';
+		$this->s__criterios_filtrado['activo']['valor'] =  $activo;
+		$respuesta->set($activo);	
+	}		
+
+	function ajax__get_dato_filtro_historico($historico, toba_ajax_respuesta $respuesta)
+	{
+		$this->s__criterios_filtrado['historico']['condicion'] =  'es_igual_a';
+		$this->s__criterios_filtrado['historico']['valor'] =  $historico;
+		$respuesta->set($historico);	
 	}	
+
+
+
 }
 
 ?>
--- php/listados/ei_filtro_listado_cargos.php
+++ php/listados/ei_filtro_listado_cargos.php
@@ -15,16 +15,16 @@ class ei_filtro_listado_cargos extends sicd_ei_filtro
 			if (!es_inicial) 
 			{
 		        idtipo_cargo = this.ef('idtipo_cargo').get_estado();
-                this.controlador.ajax('get_dato_filtro_idtipo_cargo', idtipo_cargo, this, this.actualizar_datos); 
+		                this.controlador.ajax('get_dato_filtro_idtipo_cargo', idtipo_cargo, this, this.actualizar_datos); 
 		    }
 		}		
-
+		
 		{$this->objeto_js}.evt__idtipo_hora__procesar = function(es_inicial)
 		{
 			if (!es_inicial) 
 			{
 		        idtipo_hora = this.ef('idtipo_hora').get_estado();
-                this.controlador.ajax('get_dato_filtro_idtipo_hora', idtipo_hora, this, this.actualizar_datos); 
+		                this.controlador.ajax('get_dato_filtro_idtipo_hora', idtipo_hora, this, this.actualizar_datos); 
 		    }
 		}
 			
@@ -34,7 +34,7 @@ class ei_filtro_listado_cargos extends sicd_ei_filtro
 			if (!es_inicial) 
 			{
 		        identidad = this.ef('identidad').get_estado();
-                this.controlador.ajax('get_dato_filtro_identidad', identidad, this, this.actualizar_datos); 
+		                this.controlador.ajax('get_dato_filtro_identidad', identidad, this, this.actualizar_datos); 
 		    }
 		}
 		
@@ -43,7 +43,7 @@ class ei_filtro_listado_cargos extends sicd_ei_filtro
 			if (!es_inicial) 
 			{
 		        cantidad_horas = this.ef('cantidad_horas').get_estado();
-                this.controlador.ajax('get_dato_filtro_cantidad_horas', cantidad_horas, this, this.actualizar_datos); 
+		                this.controlador.ajax('get_dato_filtro_cantidad_horas', cantidad_horas, this, this.actualizar_datos); 
 		    }
 		}
 		
@@ -52,8 +52,11 @@ class ei_filtro_listado_cargos extends sicd_ei_filtro
 			if (!es_inicial) 
 			{
 		        fecha_inicio = this.ef('fecha_inicio').fecha();
-		        inicio = fecha_inicio.getFullYear()+ '-' + fecha_inicio.getMonth()+1 +'-'+fecha_inicio.getDate();
-                this.controlador.ajax('get_dato_filtro_fecha_inicio', inicio, this, this.actualizar_datos); 
+		        mes =  fecha_inicio.getMonth()+1;
+		        inicio = fecha_inicio.getFullYear()+ '-' +mes+'-'+fecha_inicio.getDate();
+		        inicio_mostrar = fecha_inicio.getDate()+'/'+mes+'/'+fecha_inicio.getFullYear();
+		        this.controlador.ajax('get_dato_filtro_fecha_inicio', inicio, this, this.actualizar_datos); 
+		        this.controlador.ajax('get_dato_filtro_fecha_inicio_mostrar', inicio_mostrar, this, this.actualizar_datos); 
 		    }
 		}
 		
@@ -62,17 +65,76 @@ class ei_filtro_listado_cargos extends sicd_ei_filtro
 			if (!es_inicial) 
 			{
 		        fecha_fin = this.ef('fecha_fin').fecha();
-		         fin = fecha_fin.getFullYear()+ '-' + fecha_fin.getMonth()+1 +'-'+fecha_fin.getDate();
-                this.controlador.ajax('get_dato_filtro_fecha_fin', fin, this, this.actualizar_datos); 
+		        mes =  fecha_fin.getMonth()+1 ;
+		        fin = fecha_fin.getFullYear()+ '-' +mes+'-'+fecha_fin.getDate();
+		        fin_mostrar = fecha_fin.getDate()+'/'+mes+'/0'+fecha_fin.getFullYear()
+
+		        this.controlador.ajax('get_dato_filtro_fecha_fin', fin, this, this.actualizar_datos); 
+		        this.controlador.ajax('get_dato_filtro_fecha_fin_mostrar', fin_mostrar, this, this.actualizar_datos); 
 		    }
 		}
-
+		
 		{$this->objeto_js}.actualizar_datos = function(datos)
-        {                         
-        }
+		        {                         
+		        }
+		//---- Procesamiento de EFs --------------------------------
+		
+		{$this->objeto_js}.evt__tipo__procesar = function(es_inicial)
+		{
+			tipo = this.ef('tipo').get_estado();
+			if (!es_inicial) 
+			{
+		        this.controlador.ajax('get_dato_filtro_tipo', tipo, this, this.actualizar_datos);
+		    }
+	    	if (tipo=='nopar')
+	       	{
+	       		this.ef('idtipo_hora').ocultar();
+	       		this.ef('idtipo_cargo').ocultar();
+	       	}
+	       	
+	       	if (tipo=='hora')
+	       	{
+	       		this.ef('idtipo_hora').mostrar();
+	       		this.ef('idtipo_cargo').ocultar();
+	       	}
+	       	if (tipo=='cargo')
+	       	{
+	       		this.ef('idtipo_hora').ocultar();
+	       		this.ef('idtipo_cargo').mostrar();
+	       	}
+
+		}	
+		{$this->objeto_js}.evt__activo__procesar = function(es_inicial)
+		{
+			if (!es_inicial) 
+			{
+		        activo = 'false';
+		        if(this.ef('activo').get_estado() ==1)
+		        {
+		        	activo = 'true';
+		        }
+		         this.controlador.ajax('get_dato_filtro_activo', activo, this, this.actualizar_datos); 
+		    }
+
+		}		
+
+		{$this->objeto_js}.evt__historico__procesar = function(es_inicial)
+		{
+			if (!es_inicial) 
+			{
+		        historico = 'false';
+		        if(this.ef('historico').get_estado() ==1)
+		        {
+		        	historico = 'true';
+		        }
+
+		        this.controlador.ajax('get_dato_filtro_historico', historico, this, this.actualizar_datos); 
+		    }
+
+		}
 		";
 	}
 
-}
 
+}
 ?>
